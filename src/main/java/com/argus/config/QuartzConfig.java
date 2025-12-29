201b3f68b7d7b0575ddfc3365e6f32d0f30d3cd2
Tran Hong Duc <tranhongduc_t67@hus.edu.vn>
2025-12-29 18:25:58 +0700

    chore(quartz): add db connection for quartz scheduler

---
+++ src/main/java/com/argus/config/QuartzConfig.java
@@ -0,0 +1,19 @@
+package com.argus.config;
+
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class QuartzConfig {
+    @Bean
+    @QuartzDataSource
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource quartzDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
